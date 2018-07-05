637bb7b7c8f5beed8890aa73292d5f3a8ae3ea2b
Steven H Johnson <shjohnson.pi@gmail.com>
2018-07-05 19:09:07 -0400

    Add initial admin/models for core.

--- cae_web_core/admin.py
+++ cae_web_core/admin.py
@@ -1,3 +1,13 @@
+"""CAE Web Admin"""
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room
+
+# TODO: Each pluggable app should have it's own admin?
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['name', 'capacity', 'room_type']
+    list_filter = ['room_type']
+
+admin.site.register(Room, RoomAdmin)
--- cae_web_core/models.py
+++ cae_web_core/models.py
@@ -1,3 +1,22 @@
+"""CAE Web Core Models"""
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    """Room"""
+    # TODO: Have type of room be dynamic for extensibility. (Another table)
+    ROOM_TYPE_CLASS = 1
+    ROOM_TYPE_COMPUTER = 2
+    ROOM_TYPE_BREAKOUT = 3
+    ROOM_TYPE_SPECIAL = 4
+    ROOM_TYPE_CHOICES = (
+        (ROOM_TYPE_CLASS, "Classroom"),
+        (ROOM_TYPE_COMPUTER, "Computer Classroom"),
+        (ROOM_TYPE_BREAKOUT, "Breakout Room"),
+        (ROOM_TYPE_SPECIAL, "Special Room"),
+    )
+    name = models.CharField(max_length=10)
+    capacity = models.PositiveSmallIntegerField()
+    room_type = models.PositiveSmallIntegerField(
+        choices=ROOM_TYPE_CHOICES,
+    )
