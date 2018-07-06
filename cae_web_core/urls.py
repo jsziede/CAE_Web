eec2dbff1a84b2197a8206f1391740f12af9284d
Steven H Johnson <shjohnson.pi@gmail.com>
2018-07-05 23:36:51 -0400

    Add a basic calendar to test.

--- cae_web_core/urls.py
+++ cae_web_core/urls.py
@@ -2,7 +2,7 @@
 Urls for CAE_Web Core app.
 """
 
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
@@ -10,5 +10,8 @@ from . import views
 app_name = 'cae_web_core'
 urlpatterns = [
 # Home page.
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    # TODO: Note, these schedule api urls are public. Should they be permission based?
+    url(r'^schedule/', include('schedule.urls', namespace='schedule')),
+    url(r'^calendar-test/$', views.calendar_test, name='calendar_test'),
 ]
--- cae_web_core/views.py
+++ cae_web_core/views.py
@@ -10,3 +10,7 @@ def index(request):
     Root project url.
     """
     return render(request, 'cae_web_core/index.html')
+
+
+def calendar_test(request):
+    return render(request, "cae_web_core/calendar_test.html")
