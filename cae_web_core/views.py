efa80727f0d7945e2fca4492148c5a731035c017
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-07-17 21:14:10 -0400

    Update views to use template response

--- cae_web_core/views.py
+++ cae_web_core/views.py
@@ -2,15 +2,15 @@
 Views for CAE_Web Core App.
 """
 
-from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 
 def index(request):
     """
     Root project url.
     """
-    return render(request, 'cae_web_core/index.html')
+    return TemplateResponse(request, 'cae_web_core/index.html', {})
 
 
 def calendar_test(request):
-    return render(request, "cae_web_core/calendar_test.html")
+    return TemplateResponse(request, 'cae_web_core/calendar_test.html', {})
