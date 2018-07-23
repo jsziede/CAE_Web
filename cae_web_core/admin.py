7b286b57f0b3356d4b0d20cf100560d4e8dce5b5
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-07-23 15:38:28 -0400

    Move "core models" up to workspace project
    These models are to be referenced in multiple projects.
    It didn't make sense to keep them here instead of the workspace,
    which is supposed to be the "core" of all other projects.

--- cae_web_core/admin.py
+++ cae_web_core/admin.py
@@ -7,82 +7,8 @@ from django.contrib import admin
 from . import models
 
 
-class DepartmentAdmin(admin.ModelAdmin):
-    # Fields to display in admin list view.
-    list_display = ('name',)
-
-    # Fields to search in admin list view.
-    search_fields = ['name',]
-
-    # Read only fields for admin detail view.
-    readonly_fields = ('date_created', 'date_modified')
-
-    # Organize fieldsets for admin detail view.
-    fieldsets = (
-        (None, {
-            'fields': ('name',)
-        }),
-        ('Advanced', {
-            'classes': ('collapse',),
-            'fields': ('date_created', 'date_modified',),
-        }),
-    )
-
-
-class RoomTypeAdmin(admin.ModelAdmin):
-    # Fields to display in admin list view.
-    list_display = ('name',)
-
-    # Fields to search in admin list view.
-    search_fields = ['name',]
-
-    # Read only fields for admin detail view.
-    readonly_fields = ('date_created', 'date_modified')
-
-    # Organize fieldsets for admin detail view.
-    fieldsets = (
-        (None, {
-            'fields': ('name',)
-        }),
-        ('Advanced', {
-            'classes': ('collapse',),
-            'fields': ('date_created', 'date_modified',),
-        }),
-    )
-
 class RoomEventInline(admin.TabularInline):
     model = models.RoomEvent
 
-class RoomAdmin(admin.ModelAdmin):
-    # Fields to display in admin list view.
-    list_display = ('name', 'capacity', 'room_type',)
 
-    # Fields to filter by in admin list view.
-    list_filter = ('room_type', 'department',)
-
-    # Fields to search in admin list view.
-    search_fields = ['name', 'capacity',]
-
-    # Read only fields for admin detail view.
-    readonly_fields = ('date_created', 'date_modified')
-
-    # Organize fieldsets for admin detail view.
-    fieldsets = (
-        (None, {
-            'fields': (
-                'name', 'room_type', 'department', 'capacity',
-            )
-        }),
-        ('Advanced', {
-            'classes': ('collapse',),
-            'fields': ('date_created', 'date_modified',),
-        }),
-    )
-
-    inlines = [RoomEventInline]
-
-
-admin.site.register(models.Department, DepartmentAdmin)
-admin.site.register(models.RoomType, RoomTypeAdmin)
-admin.site.register(models.Room, RoomAdmin)
 admin.site.register(models.RoomEvent)
