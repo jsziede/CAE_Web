afec0137cfd60824732cbe1f07c70e14849ba10f
jbn6294 <joshua.d.sziede@wmich.edu>
2019-01-24 03:06:30 -0500

    Gets RoomCheckout models for the view to show on the attendants homepage.

--- cae_web_attendants/views.py
+++ cae_web_attendants/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template.response import TemplateResponse
+from . import models
 
+# Home page of the CAE_Web_Attendants app.
 def attendants(request):
-    return TemplateResponse(request, 'cae_web_attendants/attendants.html', {})
+    # Gets all the objects from the RoomCheckout model to show on the home page.
+    # perhaps only a certain number of models should be retrieved instead of all?
+    room_checkout_list = models.RoomCheckout.objects.all()
+    return TemplateResponse(request, 'cae_web_attendants/attendants.html', {
+        'room_checkout_list': room_checkout_list,
+    })
