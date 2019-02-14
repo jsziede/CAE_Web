6b7ae8e71a58089c2da038460846dee10941d991
jbn6294 <joshua.d.sziede@wmich.edu>
2019-02-13 21:13:22 -0500

    Split room checkout list into navigable pages.

--- cae_web_attendants/views.py
+++ cae_web_attendants/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.core.paginator import Paginator
+
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 from . import forms, models
 from cae_home import models as cae_home_models
@@ -12,6 +15,10 @@ def attendants(request):
 	# Gets all the objects from the RoomCheckout model to show on the home page.
 	# perhaps only a certain number of models should be retrieved instead of all?
 	room_checkout_list = models.RoomCheckout.objects.all()
+	paginator = Paginator(room_checkout_list, 25)
+	page = request.GET.get('page')
+	room_checkouts = paginator.get_page(page)
+
 	students = cae_home_models.WmuUser.objects.all()
 	rooms = cae_home_models.Room.objects.all()
 	form = forms.RoomCheckoutForm()
@@ -23,7 +30,7 @@ def attendants(request):
 			checkout = form.save()
 
 	return TemplateResponse(request, 'cae_web_attendants/attendants.html', {
-		'room_checkout_list': room_checkout_list,
+		'room_checkouts': room_checkouts,
 		'students': students,
 		'rooms': rooms,
 		'form': form,
