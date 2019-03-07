c9561404be4af3027db9a3ba2545b55a51438f91
jbn6294 <joshua.d.sziede@wmich.edu>
2019-03-07 12:14:42 -0500

    Convert checkout list from div to table using proper styles. Add links to sort table by each column in ascending or descending order. Use success and error panels to properly give feedback to the user when submitting a form. Minor user interface tweaks.

--- cae_web_attendants/static/cae_web_attendants/js/select_default_user.js
+++ cae_web_attendants/static/cae_web_attendants/js/select_default_user.js
@@ -1,5 +1,3 @@
-//loops through all the employee names and compares each to the currently logged in user to find a match.
-//if a match is found, it becomes the default value of the employee input for the room checkout field.
 let selectDefaultUser = function(userId) {
     let employees = document.getElementById("id_employee");
     for (i = 0; i < employees.options.length; i++) {
--- cae_web_attendants/views.py
+++ cae_web_attendants/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models.functions import Lower
 
 import json
 from django.core.serializers.json import DjangoJSONEncoder
@@ -12,9 +13,25 @@ from cae_home import models as cae_home_models
 # Gets all the objects from the RoomCheckout model to show on the home page and provides a form to submit a new room checkout
 @login_required
 def attendants(request):
+	# Determines whether or not a new checkout submission was successful.
+	# This variable is checked in the template language to determine the color of the panel heading.
+	success = 0
+
+	# Allows user to change the sorting of the room checkout table.
+	# Default is by date from newest checkout to oldest.
+	order_by = request.GET.get('order_by')
+	direction = request.GET.get('direction')
+	ordering = order_by
+
+	if direction == 'desc':
+		ordering = '-{}'.format(ordering)
+
 	# Splits the room checkout list into pages using GET data
 	checkouts_per_page = 25
-	room_checkout_list = models.RoomCheckout.objects.all()
+	if order_by and direction:
+		room_checkout_list = models.RoomCheckout.objects.all().order_by(ordering)
+	else:
+		room_checkout_list = models.RoomCheckout.objects.all()
 	paginator = Paginator(room_checkout_list, checkouts_per_page)
 	page = request.GET.get('page')
 	room_checkouts = paginator.get_page(page)
@@ -29,6 +46,9 @@ def attendants(request):
 
 		if form.is_valid():
 			checkout = form.save()
+			success = 1
+		else:
+			success = -1
 
 	return TemplateResponse(request, 'cae_web_attendants/attendants.html', {
 		'room_checkouts': room_checkouts,
@@ -36,4 +56,7 @@ def attendants(request):
 		'rooms': rooms,
 		'form': form,
 		'counter': room_checkout_loop_counter,
+		'order_by': order_by,
+		'direction': direction,
+		'success': success,
 	})
