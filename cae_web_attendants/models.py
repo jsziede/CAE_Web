9628edfa37c8e3f78a0848ac05e619a89422c691
jbn6294 <joshua.d.sziede@wmich.edu>
2019-03-13 17:39:37 -0400

    Create open/close checklist models.

--- cae_web_attendants/models.py
+++ cae_web_attendants/models.py
@@ -4,12 +4,12 @@ Models for CAE Web Attendants app.
 from django.utils import timezone
 from django.db import models
 
+# Information needed in order for an attendant to reserve a room for students' use
 class RoomCheckout(models.Model):
     # Primary key
     checkout = models.AutoField(primary_key=True)
 
     # Foreign keys
-    # check if these are supposed to be cascaded
     room = models.ForeignKey('cae_home.Room', on_delete=models.CASCADE)
     employee = models.ForeignKey('cae_home.User', on_delete=models.CASCADE)
     student = models.ForeignKey('cae_home.WmuUser', on_delete=models.CASCADE)
@@ -24,5 +24,42 @@ class RoomCheckout(models.Model):
     class Meta:
         verbose_name = "Room Checkout"
         verbose_name_plural = "Room Checkouts"
-        # check if this needs to be ordered by date or not
         ordering = ('-checkout_date',)
+
+# Acts as a template for room checklists
+class OpenCloseChecklist(models.Model):
+    # Primary key
+    checklist = models.AutoField(primary_key=True)
+
+    # Foreign keys
+    employee = models.ForeignKey('cae_home.User', on_delete=models.CASCADE)
+    checklist_item = models.ForeignKey('ChecklistItem', on_delete=models.CASCADE)
+
+    # Fields specific to RoomCheckout model
+    name = models.TextField()
+
+    # Self-setting/Non-user-editable fields.
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Checklist Template"
+        verbose_name_plural = "Checklist Templates"
+        ordering = ('checklist',)
+
+# Contains the actual tasks on the checklist, which can vary by room or checklist type (such as opening vs closing checklists)
+class ChecklistItem(models.Model):
+    # Primary key
+    checklist_item = models.AutoField(primary_key=True)
+
+    # Fields specific to RoomCheckout model
+    task = models.TextField()
+
+    # Self-setting/Non-user-editable fields.
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Checklist Item"
+        verbose_name_plural = "Checklist Items"
+        ordering = ('checklist_item',)
