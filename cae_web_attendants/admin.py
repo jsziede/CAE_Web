e2fb9bd455859f4de54aaee4f3ae3f2d03bd238d
jbn6294 <joshua.d.sziede@wmich.edu>
2019-03-20 20:07:49 -0400

    Register checklist models to Django Admin.

--- cae_web_attendants/admin.py
+++ cae_web_attendants/admin.py
@@ -15,3 +15,33 @@ class RoomCheckoutAdmin(admin.ModelAdmin):
     readonly_fields = ('checkout', 'date_created', 'date_modified')
 
 admin.site.register(RoomCheckout, RoomCheckoutAdmin)
+
+class OpenCloseChecklistAdmin(admin.ModelAdmin):
+    # Fields to display in admin list view.
+    list_display = ('checklist', 'employee', 'checklist_item', 'name', 'date_created', 'date_modified')
+
+    # Fields to filter by in admin list view.
+    list_filter = ('checklist', 'employee', 'name')
+
+    # Fields to search in admin list view.
+    search_fields = ['checklist', 'employee', 'name']
+
+    # Read only fields for admin detail view.
+    readonly_fields = ('checklist', 'date_created', 'date_modified')
+
+admin.site.register(OpenCloseChecklist, OpenCloseChecklistAdmin)
+
+class ChecklistItemAdmin(admin.ModelAdmin):
+    # Fields to display in admin list view.
+    list_display = ('checklist_item', 'task', 'date_created', 'date_modified')
+
+    # Fields to filter by in admin list view.
+    list_filter = ('checklist_item', 'task')
+
+    # Fields to search in admin list view.
+    search_fields = ['checklist_item', 'task']
+
+    # Read only fields for admin detail view.
+    readonly_fields = ('checklist_item', 'date_created', 'date_modified')
+
+admin.site.register(ChecklistItem, ChecklistItemAdmin)
