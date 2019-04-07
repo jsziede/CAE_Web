b43ee704c20ff61677e6b8634e4b946eff6c620f
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-04-07 19:03:18 -0400

    Update work_log index display

--- cae_web_work_log/views.py
+++ cae_web_work_log/views.py
@@ -19,52 +19,74 @@ def index(request):
     Work log index view.
     """
     # Pull models from database.
+    set_display_name = 'All Log Entries'
+
     complex_query = (
         (
             Q(name="CAE Admin") | Q(name="CAE Programmer")
         )
     )
-    groups = Group.objects.filter(complex_query)
-    timeframes = models.TimeFrameType.objects.all()
-    log_sets = models.WorkLogSet.objects.filter(group__in=groups)
+    form_filter_groups = Group.objects.filter(complex_query)
+    form_filter_timeframes = models.TimeFrameType.objects.all()
+    log_sets = models.WorkLogSet.objects.filter(group__in=form_filter_groups)
     log_entries = models.WorkLogEntry.objects.filter(log_set__in=log_sets)
 
     # Get filter form.
     form = forms.LogFilterForm(
-        groups=groups,
-        timeframes=timeframes,
+        groups=form_filter_groups,
+        timeframes=form_filter_timeframes,
     )
 
     # Check if request is post.
     if request.method == 'POST':
-        form = forms.LogFilterForm(request.POST, groups=groups, timeframes=timeframes)
+        form = forms.LogFilterForm(request.POST, groups=form_filter_groups, timeframes=form_filter_timeframes)
         if form.is_valid():
-            group_filter = form.cleaned_data['group']
-            timeframe_filter = form.cleaned_data['timeframe']
+            group_filter_submit_value = form.cleaned_data['group']
+            timeframe_filter_submit_value = form.cleaned_data['timeframe']
 
-            if group_filter == 'All':
-                group_filter = groups
+            # Parse group filter.
+            if group_filter_submit_value == 'All':
+                group_filter = form_filter_groups
             else:
-                group_filter = Group.objects.filter(name=group_filter)
+                group_filter = Group.objects.filter(name=group_filter_submit_value)
+                set_display_name = '{0} Log Entries'.format(group_filter.first().name)
 
-            if timeframe_filter == 'All':
-                timeframe_filter = timeframes
+            # Parse timeframe filter.
+            if timeframe_filter_submit_value == 'All':
+                timeframe_filter = form_filter_timeframes
             else:
                 timeframe_filter = models.TimeFrameType.objects.filter(
-                    name=models.TimeFrameType.get_int_from_name(timeframe_filter)
+                    name=models.TimeFrameType.get_int_from_name(timeframe_filter_submit_value)
                 )
+
+                if group_filter_submit_value == 'All':
+                    set_display_name = 'All {0} Log Entries'.format(
+                        models.TimeFrameType.get_name_from_int(timeframe_filter.first().name),
+                    )
+                else:
+                    set_display_name = '{0} {1} Log Entries'.format(
+                        models.TimeFrameType.get_name_from_int(timeframe_filter.first().name),
+                        group_filter.first().name,
+                    )
 
             # Update log display based on filters.
             log_sets = models.WorkLogSet.objects.filter(group__in=group_filter, timeframe_type__in=timeframe_filter)
             log_entries = models.WorkLogEntry.objects.filter(log_set__in=log_sets)
 
-    # Handle for non-post request.
+
+    # Get list of users with associated logs.
+    entry_user_list = []
+    for entry in log_entries:
+        if entry.user not in entry_user_list:
+            entry_user_list.append(entry.user)
+
+    # Render template to user.
     return TemplateResponse(request, 'cae_web_work_log/index.html', {
         'form': form,
-        'groups': groups,
-        'timeframes': timeframes,
         'log_sets': log_sets,
         'log_entries': log_entries,
+        'user_list': entry_user_list,
+        'group_display_name': set_display_name,
     })
 
 
@@ -105,6 +127,8 @@ def create_entry(request):
     if default_group is not None:
         visible_log_sets = models.WorkLogSet.objects.filter(group=default_group)
         form.fields['log_set'].queryset = visible_log_sets
+    else:
+        visible_log_sets = models.WorkLogSet.objects.all()
 
     # Check if request is post.
     if request.method == 'POST':
@@ -132,6 +156,6 @@ def create_entry(request):
             messages.error(request, 'Failed to create log entry.')
 
     # Handle for non-post request.
-    return TemplateResponse(request, 'cae_web_work_log/log_entry_form.html', {
+    return TemplateResponse(request, 'cae_web_work_log/form.html', {
         'form': form,
     })
