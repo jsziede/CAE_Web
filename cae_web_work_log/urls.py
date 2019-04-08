4244892c4729f8ab95b6ab2c32caa1c8f05641b7
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-04-07 21:12:39 -0400

    Implement edit functionality for work_log

--- cae_web_work_log/urls.py
+++ cae_web_work_log/urls.py
@@ -10,6 +10,6 @@ from . import views
 app_name = 'cae_web_work_log'
 urlpatterns = [
     url(r'^employee/work_log/$', views.index, name='index'),
-    url(r'^employee/work_log/create/$', views.create_entry, name='create'),
-    # url(r'^employee/work_log/(?P<pk>[0-9]+)/$', views.work_log, name='work_log'),
+    url(r'^employee/work_log/create/$', views.modify_entry, name='create_entry'),
+    url(r'^employee/work_log/edit/(?P<pk>[0-9]+)/$', views.modify_entry, name='edit_entry'),
 ]
--- cae_web_work_log/views.py
+++ cae_web_work_log/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 from django.db.models import Q
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils import timezone
@@ -49,7 +50,6 @@ def index(request):
                 group_filter = form_filter_groups
             else:
                 group_filter = Group.objects.filter(name=group_filter_submit_value)
-                set_display_name = '{0} Log Entries'.format(group_filter.first().name)
 
             # Parse timeframe filter.
             if timeframe_filter_submit_value == 'All':
@@ -59,20 +59,23 @@ def index(request):
                     name=models.TimeFrameType.get_int_from_name(timeframe_filter_submit_value)
                 )
 
-                if group_filter_submit_value == 'All':
-                    set_display_name = 'All {0} Log Entries'.format(
-                        models.TimeFrameType.get_name_from_int(timeframe_filter.first().name),
-                    )
+            # Set log set display name.
+            if group_filter_submit_value != 'All':
+                if timeframe_filter_submit_value == 'All':
+                    set_display_name = '{0} Log Entries'.format(group_filter.first().name)
                 else:
                     set_display_name = '{0} {1} Log Entries'.format(
                         models.TimeFrameType.get_name_from_int(timeframe_filter.first().name),
                         group_filter.first().name,
                     )
+            elif timeframe_filter_submit_value != 'All':
+                set_display_name = 'All {0} Log Entries'.format(
+                    models.TimeFrameType.get_name_from_int(timeframe_filter.first().name),
+                )
 
             # Update log display based on filters.
             log_sets = models.WorkLogSet.objects.filter(group__in=group_filter, timeframe_type__in=timeframe_filter)
             log_entries = models.WorkLogEntry.objects.filter(log_set__in=log_sets)
-
 
     # Get list of users with associated logs.
     entry_user_list = []
@@ -91,10 +94,17 @@ def index(request):
 
 
 @login_required
-def create_entry(request):
+def modify_entry(request, pk=None):
     """
-    Work log index view.
+    Create/edit view for a log entry.
     """
+    if pk:
+        # Pk passed. Attempt to get model for editing.
+        log_entry = get_object_or_404(models.WorkLogEntry, id=pk)
+        form = forms.LogEntryForm(instance=log_entry)
+    else:
+        log_entry = None
+
     # Set initial form data.
     entry_date = timezone.now().date()
     user = request.user
@@ -115,13 +125,15 @@ def create_entry(request):
     if default_group is not None:
         log_set = models.WorkLogSet.objects.get(timeframe_type=timeframe, group=default_group)
 
-    initial_data = {
-        'user': user,
-        'entry_date': entry_date,
-        'log_set': log_set,
-    }
+    # Handle for new entry.
+    if not pk:
+        initial_data = {
+            'user': user,
+            'entry_date': entry_date,
+            'log_set': log_set,
+        }
 
-    form = forms.LogEntryForm(initial=initial_data)
+        form = forms.LogEntryForm(initial=initial_data)
 
     # Limit visible log sets based on user group.
     if default_group is not None:
@@ -132,7 +144,10 @@ def create_entry(request):
 
     # Check if request is post.
     if request.method == 'POST':
-        form = forms.LogEntryForm(request.POST, initial=initial_data)
+        if pk:
+            form = forms.LogEntryForm(instance=log_entry, data=request.POST)
+        else:
+            form = forms.LogEntryForm(request.POST, initial=initial_data)
         form.fields['log_set'].queryset = visible_log_sets
 
         if form.is_valid():
@@ -146,10 +161,13 @@ def create_entry(request):
 
             else:
                 # Form is valid. Saving.
-                entry = form.save()
+                log_entry = form.save()
 
                 # Render response for user.
-                messages.success(request, 'Successfully created log entry ({0}).'.format(entry))
+                if pk:
+                    messages.success(request, 'Successfully modified log entry ({0}).'.format(log_entry))
+                else:
+                    messages.success(request, 'Successfully created log entry ({0}).'.format(log_entry))
                 return HttpResponseRedirect(reverse('cae_web_work_log:index'))
 
         else:
@@ -158,4 +176,5 @@ def create_entry(request):
     # Handle for non-post request.
     return TemplateResponse(request, 'cae_web_work_log/form.html', {
         'form': form,
+        'log_entry': log_entry,
     })
