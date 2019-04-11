bfd8faedd28061b1864e53dc242e0e60ec578617
jbn6294 <joshua.d.sziede@wmich.edu>
2019-04-10 22:13:01 -0400

    Merge attendant template tags into one so data can be properly provided from the template.

--- cae_web_attendants/templatetags/attendant_template_tags.py
+++ cae_web_attendants/templatetags/attendant_template_tags.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter(name='phone_number')
 def phone_number(list, index):
     """
-    Convert a 10 character string into (xxx) xxx-xxxx.
+    Converts a PhoneNumber object into a string with the format (xxx) xxx-xxxx.
     """
     number = str(list[index])
     if(number == "None"):
